e0aa4a26bfe4fbdd1a238b075b0fdeab4e89c9c8
Vishal Singh <vsvishalrana2020@gmail.com>
2026-01-16 11:32:09 +0530

    first pattern

--- StarPattern.java
+++ StarPattern.java
@@ -1,7 +1,7 @@
 public class StarPattern{
     public static void main(String[] args) {
-        int n = 5; // number of rows
-        for (int i = 1; i <= n; i++) {
+         // number of rows
+        for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
