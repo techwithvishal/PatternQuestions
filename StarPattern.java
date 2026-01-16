aea9e7a6fff15ff66ebf989ab7cae4aef7051dd9
Vishal Singh <vsvishalrana2020@gmail.com>
2026-01-16 12:12:47 +0530

    second pattern in same file

--- StarPattern.java
+++ StarPattern.java
@@ -1,11 +1,30 @@
-public class StarPattern{
-    public static void main(String[] args) {
-         // number of rows
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
+
+//  --------Right Triangle ------
+
+//public class StarPattern{
+//    public static void main(String[] args) {
+//         // number of rows
+//        for (int i = 1; i <= 5; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+
+//-------Reverse Right Triangle---------
+
+public  class StarPattern {
+    public static void main(String[] args){
+
+        for(int i= 1; i<=5; i++){
+
+            for(int j=5; j>=i; j--){
+                System.out.print("*");
             }
-            System.out.println();
+            System.out.println("");
         }
     }
 }
