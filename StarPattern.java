18f01ba38f736a0dc681cd8e6efd8ffa74bb5a50
Vishal Singh <vsvishalrana2020@gmail.com>
2026-01-17 16:27:16 +0530

    new pattern in same file

--- StarPattern.java
+++ StarPattern.java
@@ -16,15 +16,35 @@
 
 //-------Reverse Right Triangle-----------------------------------------------------------------------------------
 
-public  class StarPattern {
-    public static void main(String[] args){
+//public  class StarPattern {
+//    public static void main(String[] args){
+//
+//        for(int i= 1; i<=5; i++){
+//
+//            for(int j=5; j>=i; j--){
+//                System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
+//    }
+//}
 
-        for(int i= 1; i<=5; i++){
+//-------------- dono ka combination half right triangle + half reverse triangle
 
-            for(int j=5; j>=i; j--){
+public class StarPattern {
+    public static void main (String[] args) {
+        for (int i=1; i<=5; i++){
+            for(int j=1; j<=i; j++){
                 System.out.print("*");
             }
-            System.out.println("");
+            System.out.println();
+        }
+
+        for(int i=1; i<=5; i++){
+            for(int j= 4; j>=i; j--){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
